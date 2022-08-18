f00ee056281d8404b96923f4193d9e0b5e12c364
Thomas Feuerbach <thfeuerbach@gmail.com>
2022-08-18 12:16:22 +0000

    updated config with updated BASE_URL

--- config.py
+++ config.py
@@ -4,17 +4,9 @@ from functools import lru_cache
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    env_name: str = "Local"
-    base_url: str = "http://localhost:8000"
+    env_name: str = "Prod"
+    base_url: str = "http://myshortened.link"
     db_url: str = "sqlite:///./shortener.db"
-
-    # .env file located outside app directory
-    # ENV_NAME="Development"
-    # BASE_URL="http://127.0.0.1:8000"
-    # DB_URL="sqlite:///./shortener.db"
-
-    class Config:
-        env_file = ".env"
 
 @lru_cache
 def get_settings() -> Settings:
